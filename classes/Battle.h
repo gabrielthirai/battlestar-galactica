8ba469fd23c05c41b4aa0bbd0c44fe74429b0fa5
gabrielthirai <gabrielt.hirai@gmail.com>
2023-05-18 12:21:56 -0300

    Finalizando a classe battle

--- classes/Battle.h
+++ classes/Battle.h
@@ -4,15 +4,17 @@ using namespace std;
 
 class Battle{
     private:
+        string objNome;
         int objAtaque, objDefesa, objLevel, objLife, objPontos;
     public:
-        Battle();
+        Battle(string nome, int atk, int def, int lvl, int lf, int pt);
         void set_atk(int atk); //essa função irá definir o ataque da nave.
         void set_def(int def); //essa função irá definir a defesa da nave.
         void set_lvl(int lvl); //essa função irá definir o level da nave, que será incrementado a cada fim de batalha, somente na nave vencedora.void set_lf(int lf); //essa função irá definir o life da nave, sempre mudando a cada ataque.
         void set_pt(int pt); //essa função irá definir o pontos de atributos da nave, só irá acrescentar no final da guerra, a vencedora recebe 5 extras.
         
         
+        string get_nome();// essa função irá mostrar o nome das naves
         int get_atk(); //essa função irá buscar o valor de ataque da nave.
         int get_def(); //essa função irá buscar o valor de defesa da nave.
         int get_lvl(); //essa função irá buscar o level da nave.
@@ -20,11 +22,16 @@ class Battle{
         int get_pt(); //essa função irá buscar quantos pontos a nave tem para serem distribuídos.
 
 
-        int Sorteio();
+        void RetornaDados();
 };
 
-Battle::Battle(){
-    
+Battle::Battle(string nome, int atk, int def, int lvl, int lf, int pt){
+    objNome = nome;
+    objAtaque = atk;
+    objDefesa = def;
+    objLevel = lvl;
+    objLife = lf;
+    objPontos = 5;
 }
 
 void Battle::set_atk(int atk){
@@ -63,12 +70,15 @@ int Battle::get_lvl(){
 int Battle::get_pt(){
     return objPontos;
 }
+string Battle::get_nome(){
+    return objNome;
+}
 
-
-
-int Battle::Sorteio(){
-    int valor;
-    
-    valor = rand() % 100 + 1;
-    return valor;
+void Battle::RetornaDados(){
+    cout<< "Nome: "<<get_nome();
+    cout<< "Ataque: "<<get_atk();
+    cout<< "Defesa: "<<get_def();
+    cout<< "Life: " <<get_lf();
+    cout<< "Level: "<<get_lvl();
+    cout<< "Pontos: "<<get_pt();
 }
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include <Battle.h>
+
+using namespace std;
+
